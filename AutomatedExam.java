7981d00b67b9845f5597379969b8942f094f8936
CSteinmuller <chris.dungeonmaster@gmail.com>
2017-05-30 11:58:16 -0500

    Update AutomatedExam.java
    remove cruft

--- AutomatedExam.java
+++ AutomatedExam.java
@@ -1,13 +1,4 @@
-/**
-@file AutomatedExam
-@author Christopher Steinmuller
-@date 2010-13-11
-Description: main
-Course: CS2700.01
-Paws ID: cstein1
-Programming Project #: 4
-Instructor: Duncan
-*/
+
 
 
 package AutomatedExam;
@@ -19,7 +10,6 @@ public class AutomatedExam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         Exam test = new Exam();
        
 
